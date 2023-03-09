7de13ceca9923e7e0e27c201deeac29800e4eb03
caio-rds <caiodtn@gmail.com>
2023-03-09 01:56:25 -0300

    Customers section has news divs for receive more than one customer

--- core/views/customer.py
+++ core/views/customer.py
@@ -1,13 +1,9 @@
-import json
-
-from django.shortcuts import render, redirect
 from django.template.response import TemplateResponse
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def load_customer_page(request):
     if request.method == 'GET':
-        print('alo')
         return TemplateResponse(request, 'customer.html')
 
 
@@ -18,29 +14,55 @@ def customer(request):
 
     if request.method == 'POST':
         print(request.POST)
-        return {'status': 'success', 'message': 'Customer created successfully'}
+        return JsonResponse({
+            'status': 'success',
+            'message': 'Customer created successfully'
+        })
 
     if request.method == 'DELETE':
         print(request.POST)
         return HttpResponse()
 
     if request.method == 'GET':
-        if not request.GET.get('identify'):
-            return JsonResponse({
-                'status': 'error',
-                'message': 'Não há identificador para o cliente'
-            })
+        # if not request.GET.get('identify'):
+        #     return JsonResponse({
+        #         'status': 'error',
+        #         'message': 'Não há identificador para o cliente'
+        #     })
         return JsonResponse({
             'status': 'success',
-            'customer': {
-                'name': 'John Doe',
-                'email': 'johndoe@gmail.com',
-                'phone': '123456789',
-                'cpf': '123456789',
-                'birth_date': '01/01/2000',
-                'postal_code': '21810-000',
-                'address': 'Rua Antonio Cerazio, 111',
-                'city': 'Rio de Janeiro',
-                'state': 'Rio de Janeiro'
+            'customers': {'1': {
+                    'name': 'John Doe',
+                    'email': 'johndoe@gmail.com',
+                    'phone': '123456789',
+                    'cpf': '123456789',
+                    'birth_date': '01/01/2000',
+                    'postal_code': '21810-000',
+                    'address': 'Rua Antonio Cerazio, 111',
+                    'city': 'Rio de Janeiro',
+                    'state': 'Rio de Janeiro'
+                },
+                '2': {
+                    'name': 'John Doe2',
+                    'email': 'johndoe2@gmail.com',
+                    'phone': '1234567892',
+                    'cpf': '1234567892',
+                    'birth_date': '01/01/2000',
+                    'postal_code': '21810-000',
+                    'address': 'Rua Antonio Cerazio, 111',
+                    'city': 'Rio de Janeiro',
+                    'state': 'Rio de Janeiro'
+                }
             }
+            # 'customer': {
+            #     'name': 'John Doe',
+            #     'email': 'johndoe@gmail.com',
+            #     'phone': '123456789',
+            #     'cpf': '123456789',
+            #     'birth_date': '01/01/2000',
+            #     'postal_code': '21810-000',
+            #     'address': 'Rua Antonio Cerazio, 111',
+            #     'city': 'Rio de Janeiro',
+            #     'state': 'Rio de Janeiro'
+            # }
         })
